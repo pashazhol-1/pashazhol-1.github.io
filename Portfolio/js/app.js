b4ef329e065485f13657ce9f5bc8acf148f4f700
pashazhol-1 <pashazhol51@gmail.com>
2020-05-13 23:10:40 +0300

    update

--- Portfolio/js/app.js
+++ Portfolio/js/app.js
@@ -64,4 +64,21 @@ $(function(){
         event.preventDefault();
         nav.toggleClass("show");
     });
+    
+            var btn = $('#btn_top');
+
+        $(window).scroll(function() {
+          if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+          } else {
+            btn.removeClass('show');
+          }
+        });
+
+        btn.on('click', function(e) {
+          e.preventDefault();
+          $('html, body').animate({scrollTop:0}, '300');
+        });
+    
+    
 });
