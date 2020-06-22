a00adbf039a8552daea4194d8ec55d3eb811b6f8
pashazhol-1 <pashazhol51@gmail.com>
2020-06-22 16:56:09 +0300

    update

---
+++ motion-effect241017/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/alexpopovich/pen/jGmKpM.
+
+ Made a motion blur effect with SVG technology, TweenMax.
---
+++ seo-digital/vantajs/app.js
@@ -0,0 +1,11 @@
+VANTA.NET({
+  el: "#swiper",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xffb400,
+  backgroundColor: 0x1d4f96
+})
